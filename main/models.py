f610cc00243caf96ef7385d4d987ddbf2d9828be
lizihan <lizihan@umich.edu>
2017-07-14 17:06:30 -0700

    yo

--- main/models.py
+++ main/models.py
@@ -283,7 +283,7 @@ class models(object):
 
         start_time = time.time()
         evaluate_all(self, words)
-        '''
+        
         counter = 0
         try:
             for eidx in xrange(params.epochs):
@@ -340,7 +340,7 @@ class models(object):
 
         except KeyboardInterrupt:
             print "Training interupted"
-        '''
+        
 
         end_time = time.time()
         print "total time:", (end_time - start_time)
